ec4f386e4bd7b09034e4c85e217b27247f36c602
wrc9817 <wrc9817@gmail.com>
2021-03-02 23:33:16 +0800

    part2 finished

---
+++ Lab 5/script/lab5-2.js
@@ -0,0 +1,24 @@
+window.addEventListener('load',function(){
+  document.body.querySelectorAll('*').forEach(function(node){
+    if(node.nodeType != Node.TEXT_NODE){
+      var tag = node.tagName;
+      var child = document.createElement("p");
+      child.classList.add("hoverNode");
+      child.innerHTML = tag;
+      node.appendChild(child);
+      setStyle(child);
+      child.addEventListener('click', function(){
+        alert("TagName: " + tag + " ID: " + node.id + " InnerHTML: "+ node.innerHTML);
+        console.log(tag);
+      });
+    }
+  });
+})
+
+function setStyle(e){
+  e.style.border = "solid black";
+  e.style.borderWidth = "1px";
+  e.style.display = "inline";
+  e.style.backgroundColor = "yellow";
+  e.style.width = "min-content";
+}
