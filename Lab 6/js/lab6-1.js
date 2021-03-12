1b6be1ecf877fe8ebfe8f2d00ee6e13d20da808e
wrc9817 <wrc9817@gmail.com>
2021-03-12 13:16:45 +0800

    update 6-1

--- Lab 6/js/lab6-1.js
+++ Lab 6/js/lab6-1.js
@@ -28,32 +28,27 @@ $(function () {
   });
   $("#panel-2").append(img);
   // floating preview
-  $(".img-responsive").on("mousemove", function() {
+  $(".img-responsive").on("mouseover", function() {
     var alt = $(this).attr("alt");
     var src = $(this).attr("src");
     var newsrc = src.replace("small", "medium");
     var preview = $('<div id="preview"></div>');
     var image = $('<img src = "' + newsrc + '">');
     var caption = $("<p>" + alt + "</p>");
-    preview.css(
-      "position",
-      "absolute",
-      "top",
-      "100px",
-      "left",
-      "0px",
-      "display",
-      "none",
-    );
-    $("main").prepend(preview);
-    $("#preview").prepend(image);
-    $("#preview").append(caption);
-    $("#preview").fadeIn(1000);
-    $(this).css("filter", "grayscale(100%)");
-  });
-    $("#stories img").on("mouseleave", function () {
-      $("#preview").remove();
-      $(".img-responsive").css("filter", "grayscale(0%)");
+    preview.css({
+      position:'absolute',
+      top:"360pt",
+      left:"100pt",
     });
+    preview.append(image);
+    preview.append(caption);
+    $(this).css("filter", "grayscale(100%)");
+    $(".container").append(preview);
+    $("#preview").fadeIn(1000);
+  });
+  $(".img-responsive").on("mouseleave", function () {
+    $("#preview").remove();
+    $(this).css("filter", "grayscale(0%)");
+  });
 });
 
